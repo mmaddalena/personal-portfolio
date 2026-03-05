792b05be75ae1efce247084bf0ec02fb3c04cf3f
mmaddalena <mmaddalena@fi.uba.ar>
2026-03-05 01:50:39 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/**
+ * @copyright 2026 mmaddalena
+ * @license Apache-2.0
+ */
+
+import { Button } from "./components/ui/button"
+
+export const App = () => {
+  return (
+      <Button>Click me</Button>
+  )
+}
