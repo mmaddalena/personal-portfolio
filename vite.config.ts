c88aa75dbc719bb53aba3443f82e1ba596fff2fe
mmaddalena <mmaddalena@fi.uba.ar>
2026-03-07 02:04:49 -0300

    feat: base url config added

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: { alias: { '@': path.resolve(__dirname, './src') } },
 });
