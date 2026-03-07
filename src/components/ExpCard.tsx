2626df17747cdbf7594884994be7ba597d70a35a
mmaddalena <mmaddalena@fi.uba.ar>
2026-03-07 10:45:02 -0300

    feat: stack and content updated

--- src/components/ExpCard.tsx
+++ src/components/ExpCard.tsx
@@ -20,7 +20,7 @@ export const ExpCard = ({ item }: { item: ExperienceType }) => {
       <h3 className='text-lg font-semibold mt-1'>{item.title}</h3>
 
       <p className='text-sm text-neutral-400 mb-1'>
-        Course by{' '}
+        {/* Course by{' '} */}
         <span className='font-medium text-foreground'>{item.institute}</span>
       </p>
 
